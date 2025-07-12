8812e3e135c3ea0a5ae844d0d02b3155e2d0b555
Kiliken <francescopaolomariani7@gmail.com>
2025-07-12 18:47:16 +0900

    Item Store

--- src/main.cpp
+++ src/main.cpp
@@ -6,7 +6,7 @@
  #define ASSETS "../assets/"
 
 struct Item {
-    sf::Texture texture;
+    sf::Texture *texture;
     sf::Sprite  *sprite;
     sf::String  name;
     int         price;
@@ -17,13 +17,28 @@ std::vector<Item> DeclareItems() {
 
     Item bow;
 
-    bow.texture = sf::Texture("../assets/bow.png");
-    bow.sprite = new sf::Sprite(bow.texture);
+
+    bow.texture = new sf::Texture("../assets/bow.png");
+    bow.sprite = new sf::Sprite(*bow.texture);
     bow.name = "Bow";
     bow.price = 10;
 
+    items.push_back(bow);
 
     return items;
+}
+
+Item InteractWith(std::vector<Item> items, sf::Sprite mousePos){
+    Item empty;
+
+
+    for(int i = 0; i < items.size(); i++){
+         if (items[i].sprite->getGlobalBounds().findIntersection(mousePos.getGlobalBounds())){
+            return items[i];
+         }    
+    }
+
+    return empty;
 }
 
 int main()
@@ -38,19 +53,30 @@ int main()
     sf::CircleShape shape(100.f);
     shape.setFillColor(sf::Color::Red);
 
-    
+    std::vector<Item> inventory;
+    sf::Vector2f inventoryPos[12] = {
+        {900.f,300.f},{500.f,400.f},{0.f,0.f},
+        {0.f,0.f},{0.f,0.f},{0.f,0.f},
+        {0.f,0.f},{0.f,0.f},{0.f,0.f},
+        {0.f,0.f},{0.f,0.f},{0.f,0.f},
+    };
 
     const sf::Texture cursorText("../assets/cursor.png");
     sf::Sprite cursorSprite(cursorText);
     cursorSprite.setOrigin({cursorText.getSize().x / 2.f,cursorText.getSize().y/ 2.f});
     cursorSprite.setScale({0.06f,0.06f});
+
+    const sf::Texture bgText("../assets/background.png");
+    sf::Sprite bgSprite(bgText);
+    bgSprite.setOrigin({0.f,0.f});
+    bgSprite.setScale({1.25f,1.25f});
     
     std::vector<Item> items = DeclareItems();
 
     for(int i = 0; i < items.size(); i++){
         items[i].sprite->setPosition({200.f,200.f});
-        items[i].sprite->setScale({0.25f,0.25f});
-        std::cout << items[i].name.toAnsiString() << std::endl;
+        items[i].sprite->setScale({0.10f,0.10f});
+        
     }
             
     
@@ -64,23 +90,28 @@ int main()
                 window.close();
 
             if (event->is<sf::Event::MouseButtonPressed>()){
-                if (cursorSprite.getGlobalBounds().findIntersection(shape.getGlobalBounds()))
-                std::cout << "WORKS" << std::endl;
+                Item itemClicked = InteractWith(items,cursorSprite);
+                inventory.push_back(itemClicked);
             }
         }
 
         cursorSprite.setPosition({static_cast<float>(sf::Mouse::getPosition().x) - window.getPosition().x,static_cast<float>(sf::Mouse::getPosition().y) - window.getPosition().y});
         
         
-
-
+       for(int i = 0; i < inventory.size(); i++){
+            inventory[i].sprite->setPosition(inventoryPos[i]);
+            inventory[i].sprite->setScale({0.10f,0.10f});
+        }
 
         window.clear();
+        window.draw(bgSprite);
         window.draw(shape);
-        window.draw(cursorSprite);
+        
 
         for(int i = 0; i < items.size(); i++)
             window.draw(*items[i].sprite);
+
+        window.draw(cursorSprite);
         
         window.display();
     }
